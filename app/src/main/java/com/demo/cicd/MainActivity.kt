5948aa252a5a065f54123ab2fe9f8d819304845e
Arashjit Singh <arashjit.singhh@gmail.com>
2023-02-09 10:53:58 +0000

    Added Appceneter Analytics

--- app/src/main/java/com/demo/cicd/MainActivity.kt
+++ app/src/main/java/com/demo/cicd/MainActivity.kt
@@ -2,10 +2,24 @@ package com.demo.cicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo.cicd.databinding.ActivityMainBinding
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCenter.start(application,"49b9efa3-e1e1-48eb-9884-d2398de6e643",Analytics::class.java,Crashes::class.java)
+
+        binding.btnCrashApplication.setOnClickListener {
+            Crashes.generateTestCrash()
+        }
     }
 }
